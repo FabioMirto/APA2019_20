c5f1cee6849d916437233d8bafda902d2fe408c9
FabioMirto <56192516+FabioMirto@users.noreply.github.com>
2019-12-01 23:51:17 +0100

    Update es3.c
    controlli da fare

--- Lab7/es3.c
+++ Lab7/es3.c
@@ -5,7 +5,7 @@
 #define MAX 50
 
 typedef enum {
-    r_newpg, r_deletepg, r_deleteob, r_statpg, r_err, r_fine
+    r_newpg, r_deletepg, r_addob, r_deleteob, r_statpg, r_err, r_fine
 } comandi_e;
 
 typedef struct{
@@ -57,17 +57,17 @@ nodoPg_t newNode(pg_t val, nodoPg_t next);
 void  ListIn(tabPg_t *tabPg);
 void VectIn(tabInv_t *tabInv);
 nodoPg_t listInsHead (nodoPg_t h, pg_t val);
-void printList(tabPg_t h);
+void printList(tabPg_t *h);
 void InsDelOb(tabPg_t *tabPg, char k[6+1], char ob[MAX+1], tabInv_t *tabInv, int scelta);
+void DelOb(tabPg_t *tabPg, char k[6+1], char ob[MAX+1], tabInv_t *tabInv);
+void InsOb(tabPg_t *tabPg, char k[6+1], char ob[MAX+1], tabInv_t *tabInv);
 void selezionaDati(tabPg_t *tabPg, tabInv_t *tabInv, comandi_e codiceComando);
 comandi_e leggiComando(void);
 void minuscolo(char parola[MAX]);
 nodoPg_t listDelKey(nodoPg_t h, char k[6+1]);
 void calStat(tabPg_t *tabPg, char k[6+1], tabInv_t *tabInv);
 
-
 int main() {
-    int i;
     tabPg_t tabPg;
     tabInv_t tabInv;
     ListIn(&tabPg);
@@ -82,10 +82,10 @@ comandi_e leggiComando(void){
         comandi_e c;
         char cmd[MAX];
         char tabella[r_fine][MAX] = {
-                "new_pg", "delete_pg",
+                "new_pg", "delete_pg", "add_ob",
                 "delete_ob", "stats"
         };
-        printf("\t\t\t\t\t\t[COMANDO]\nNew_pg Delete_pg Delete_ob Stats\n"
+        printf("\t\t\t\t\t\t[COMANDO]\nNew_pg Delete_pg Add_ob Delete_ob Stats\n"
                "Fine per terminare\n");
         scanf("%s", cmd);
         minuscolo(cmd);
@@ -97,7 +97,7 @@ comandi_e leggiComando(void){
 
 void minuscolo(char parola[MAX]){
     int i, len;
-    len = strlen(parola);
+    len = (int) strlen(parola);
     for(i = 0; i < len; i++){
         parola[i] = tolower(parola[i]);
     }
@@ -120,28 +120,27 @@ void selezionaDati(tabPg_t *tabPg, tabInv_t *tabInv, comandi_e codiceComando){
                 scanf("%s %s %s %d %d %d %d %d %d", tmp.codice, tmp.personaggio, tmp.classe, &tmp.stat.hp,
                       &tmp.stat.mp, &tmp.stat.atk, &tmp.stat.def, &tmp.stat.mag, &tmp.stat.spr);
                 tabPg->headPg = listInsHead(tabPg->headPg, tmp);
+                printf("Personaggio aggiunto nella lista\n");
+                printList(tabPg);
                 codiceComando = leggiComando();
                 break;
             case r_deletepg :
-                printf("Inserire il codice di un personaggio per eliminarlo");
+                printf("Inserire il codice di un personaggio per eliminarlo\n");
                 scanf("%s", code);
                 tabPg->headPg = listDelKey(tabPg->headPg, code);
+                printf("Personaggio eliminato dalla lista\n");
+                printList(tabPg);
+                codiceComando = leggiComando();
+                break;
+            case r_addob:
+                InsOb(tabPg, code, ob, tabInv);
                 codiceComando = leggiComando();
                 break;
             case r_deleteob :
-                printf("Per aggiungere/rimuovere un oggetto dall’equipaggiamento di un personaggio\npremere rispettivamente 0 o 1\n");
-                scanf("%d", &scelta);
-                printf("Inserire il codice del personaggio\n (formato PGXXXX dove X rappresenta"
-                       "una cifra nell'intervallo 0-9) \n");
-                scanf("%s", code);
-                printf("Digita il nome dell'oggetto da inserire\n");
-                scanf("%s", ob);
-                InsDelOb(tabPg, code, ob, tabInv, scelta);
+                DelOb(tabPg, code, ob, tabInv);
                 codiceComando = leggiComando();
                 break;
             case r_statpg :
-                printf("Inserire il codice di un personaggio per calcolare le sue statistiche\n");
-                scanf("%s", code);
                 calStat(tabPg, code, tabInv);
                 codiceComando = leggiComando();
                 break;
@@ -190,15 +189,15 @@ void ListIn(tabPg_t *tabPg){
     fclose(fp);
 }
 
-void printList(tabPg_t h){
-    tabPg_t tmp;
-    for (tmp = h; tmp.headPg->next != NULL; tmp.headPg = tmp.headPg->next) {
-            printf("%s %s %s %d %d %d %d %d %d\n", tmp.headPg->val.codice,
-                   tmp.headPg->val.personaggio, tmp.headPg->val.classe,
-                   tmp.headPg->val.stat.hp,  tmp.headPg->val.stat.mp,  tmp.headPg->val.stat.atk,
-                   tmp.headPg->val.stat.def,  tmp.headPg->val.stat.mag, tmp.headPg->val.stat.spr);
-        }
+void printList(tabPg_t *h){
+    tabPg_t *x;
+    for (x = h; x->headPg->next != NULL; x->headPg = x->headPg->next) {
+            printf("%s %s %s %d %d %d %d %d %d\n", x->headPg->val.codice,
+                   x->headPg->val.personaggio, x->headPg->val.classe,
+                   x->headPg->val.stat.hp,  x->headPg->val.stat.mp,  x->headPg->val.stat.atk,
+                   x->headPg->val.stat.def,  x->headPg->val.stat.mag, x->headPg->val.stat.spr);
     }
+}
 
 void VectIn(tabInv_t *tabInv){
     int i;
@@ -232,6 +231,90 @@ nodoPg_t listDelKey(nodoPg_t h, char k[6+1]) {
     return h;
 }
 
+void InsOb(tabPg_t *tabPg, char k[6+1], char ob[MAX+1], tabInv_t *tabInv){
+    nodoPg_t x;
+    x = tabPg->headPg;
+    printf("Inserire il codice del personaggio\n(formato PGXXXX dove X rappresenta"
+           "una cifra nell'intervallo 0-9) \n");
+    scanf("%s", k);
+    int i, flag = 0;
+    if(x->val.equip.inUso == 8){
+        printf("Spazio esaurito\n");
+        return;
+    }
+    for (x = tabPg->headPg; x != NULL && flag == 0; x = x->next) {
+        if (strcmp(x->val.codice, k) == 0) {
+            flag = 1;
+        }
+    }
+        if (flag) {
+            printf("Digita il nome dell'oggetto da inserire\n");
+            scanf("%s", ob);
+            for (x = tabPg->headPg; x != NULL; x = x->next) {
+                for (i = 0; i < tabInv->nInv; i++) {
+                    if (strcmp(ob, tabInv->vettInv[i].nome) == 0) {
+                       /* strcpy(x->val.equip.vettEq[x->val.equip.inUso]->nome, tabInv->vettInv[i].nome);
+                        strcpy(x->val.equip.vettEq[x->val.equip.inUso]->tipo, tabInv->vettInv[i].tipo);
+                        x->val.equip.vettEq[x->val.equip.inUso]->stat.hp = tabInv->vettInv[i].stat.hp;
+                        x->val.equip.vettEq[x->val.equip.inUso]->stat.mp = tabInv->vettInv[i].stat.mp;
+                        x->val.equip.vettEq[x->val.equip.inUso]->stat.atk = tabInv->vettInv[i].stat.atk;
+                        x->val.equip.vettEq[x->val.equip.inUso]->stat.def = tabInv->vettInv[i].stat.def;
+                        x->val.equip.vettEq[x->val.equip.inUso]->stat.mag = tabInv->vettInv[i].stat.mag;
+                        x->val.equip.vettEq[x->val.equip.inUso]->stat.spr = tabInv->vettInv[i].stat.spr;*/
+                        x->val.equip.vettEq[x->val.equip.inUso] = &tabInv->vettInv[i];
+                        x->val.equip.inUso++;
+                    }
+                }
+            }
+        } else {
+            printf("Codice non trovato\n");
+            return;
+        }
+    }
+
+void DelOb(tabPg_t *tabPg, char k[6+1], char ob[MAX+1], tabInv_t *tabInv){
+    nodoPg_t x;
+    x = tabPg->headPg;
+    printf("Inserire il codice del personaggio\n(formato PGXXXX dove X rappresenta"
+           "una cifra nell'intervallo 0-9) \n");
+    scanf("%s", k);
+    int i, flag = 0;
+    if(x->val.equip.inUso == 0){
+        printf("Zaino vuoto\n");
+        return;
+    }
+    for (x = tabPg->headPg; x != NULL && flag == 0; x = x->next) {
+        if (strcmp(x->val.codice, k) == 0) {
+            flag = 1;
+            if (flag) {
+                printf("Digita il nome dell'oggetto da eliminare\n");
+                scanf("%s", ob);
+                for (x = tabPg->headPg; x != NULL; x = x->next) {
+                    for (i = 0; i < 8; i++) {
+                        if (strcmp(ob, x->val.equip.vettEq[i]->nome) == 0) {
+                            strcpy(x->val.equip.vettEq[i]->nome, "");
+                            strcpy(x->val.equip.vettEq[i]->tipo, "");
+                            x->val.equip.vettEq[i]->stat.hp = 0;
+                            x->val.equip.vettEq[i]->stat.mp = 0;
+                            x->val.equip.vettEq[i]->stat.atk = 0;
+                            x->val.equip.vettEq[i]->stat.def = 0;
+                            x->val.equip.vettEq[i]->stat.mag = 0;
+                            x->val.equip.vettEq[i]->stat.spr = 0;
+                            x->val.equip.inUso--;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if(!flag){
+        printf("Codice non trovato\n");
+        return;
+    }
+
+    }
+
 void InsDelOb(tabPg_t *tabPg, char k[6+1], char ob[MAX+1], tabInv_t *tabInv, int scelta){
     nodoPg_t x;
     x = tabPg->headPg;
@@ -250,37 +333,58 @@ void InsDelOb(tabPg_t *tabPg, char k[6+1], char ob[MAX+1], tabInv_t *tabInv, int
             for (x = tabPg->headPg; x != NULL; x = x->next) {
                 for (i = 0; i < tabInv->nInv; i++) {
                     if (strcmp(ob, tabInv->vettInv[i].nome) == 0) {
-                       /* strcpy(x->val.equip.vettEq[x->val.equip.inUso]->nome, tabInv->vettInv[i].nome);
-                        strcpy(x->val.equip.vettEq[x->val.equip.inUso]->tipo, tabInv->vettInv[i].tipo);
-                        x->val.equip.vettEq[x->val.equip.inUso]->stat.hp = tabInv->vettInv[i].stat.hp;
-                        x->val.equip.vettEq[x->val.equip.inUso]->stat.mp = tabInv->vettInv[i].stat.mp;
-                        x->val.equip.vettEq[x->val.equip.inUso]->stat.atk = tabInv->vettInv[i].stat.atk;
-                        x->val.equip.vettEq[x->val.equip.inUso]->stat.def = tabInv->vettInv[i].stat.def;
-                        x->val.equip.vettEq[x->val.equip.inUso]->stat.mag = tabInv->vettInv[i].stat.mag;
-                        x->val.equip.vettEq[x->val.equip.inUso]->stat.spr = tabInv->vettInv[i].stat.spr;*/
+                        /* strcpy(x->val.equip.vettEq[x->val.equip.inUso]->nome, tabInv->vettInv[i].nome);
+                         strcpy(x->val.equip.vettEq[x->val.equip.inUso]->tipo, tabInv->vettInv[i].tipo);
+                         x->val.equip.vettEq[x->val.equip.inUso]->stat.hp = tabInv->vettInv[i].stat.hp;
+                         x->val.equip.vettEq[x->val.equip.inUso]->stat.mp = tabInv->vettInv[i].stat.mp;
+                         x->val.equip.vettEq[x->val.equip.inUso]->stat.atk = tabInv->vettInv[i].stat.atk;
+                         x->val.equip.vettEq[x->val.equip.inUso]->stat.def = tabInv->vettInv[i].stat.def;
+                         x->val.equip.vettEq[x->val.equip.inUso]->stat.mag = tabInv->vettInv[i].stat.mag;
+                         x->val.equip.vettEq[x->val.equip.inUso]->stat.spr = tabInv->vettInv[i].stat.spr;*/
                         x->val.equip.vettEq[x->val.equip.inUso] = &tabInv->vettInv[i];
                         x->val.equip.inUso++;
-                        }
                     }
+                }
             }
         }
     } else {
-     if (flag) {
+        if (flag) {
             for (x = tabPg->headPg; x != NULL; x = x->next) {
                 for (i = 0; i < 8; i++) {
                     if (strcmp(ob, x->val.equip.vettEq[i]->nome) == 0) {
-                         strcpy(x->val.equip.vettEq[i]->nome, "");
-                         strcpy(x->val.equip.vettEq[i]->tipo, "");
-                         x->val.equip.vettEq[i]->stat.hp = 0;
-                         x->val.equip.vettEq[i]->stat.mp = 0;
-                         x->val.equip.vettEq[i]->stat.atk = 0;
-                         x->val.equip.vettEq[i]->stat.def = 0;
-                         x->val.equip.vettEq[i]->stat.mag = 0;
-                         x->val.equip.vettEq[i]->stat.spr = 0;
-                         x->val.equip.inUso--;
-                        return;
-                     }
+                        strcpy(x->val.equip.vettEq[i]->nome, "");
+                        strcpy(x->val.equip.vettEq[i]->tipo, "");
+                        x->val.equip.vettEq[i]->stat.hp = 0;
+                        x->val.equip.vettEq[i]->stat.mp = 0;
+                        x->val.equip.vettEq[i]->stat.atk = 0;
+                        x->val.equip.vettEq[i]->stat.def = 0;
+                        x->val.equip.vettEq[i]->stat.mag = 0;
+                        x->val.equip.vettEq[i]->stat.spr = 0;
+                        x->val.equip.inUso--;
+                    }
                 }
+                        for (i = 0; i < x->val.equip.inUso; i++) {
+                            x->val.stat.hp -= x->val.equip.vettEq[i]->stat.hp;
+                            x->val.stat.mp -= x->val.equip.vettEq[i]->stat.mp;
+                            x->val.stat.atk -= x->val.equip.vettEq[i]->stat.atk;
+                            x->val.stat.def -= x->val.equip.vettEq[i]->stat.def;
+                            x->val.stat.mag -= x->val.equip.vettEq[i]->stat.mag;
+                            x->val.stat.spr -= x->val.equip.vettEq[i]->stat.spr;
+                            if (x->val.stat.hp < 0){
+                                x->val.stat.hp = 0;
+                            }if (x->val.stat.mp < 0){
+                                x->val.stat.mp = 0;
+                            }if (x->val.stat.atk < 0){
+                                x->val.stat.atk = 0;
+                            }if (x->val.stat.def < 0){
+                                x->val.stat.def = 0;
+                            }if (x->val.stat.mag < 0){
+                                x->val.stat.mag = 0;
+                            }if (x->val.stat.spr < 0){
+                                x->val.stat.spr = 0;
+                            }
+                        }
+                        // return;
             }
         }
     }
@@ -290,6 +394,8 @@ void InsDelOb(tabPg_t *tabPg, char k[6+1], char ob[MAX+1], tabInv_t *tabInv, int
 void calStat(tabPg_t *tabPg, char k[6+1], tabInv_t *tabInv){
     int i, j, flag = 0;
     nodoPg_t x;
+    printf("Inserire il codice di un personaggio per calcolare le sue statistiche\n");
+    scanf("%s", k);
     for (x = tabPg->headPg; x != NULL && flag == 0; x = x->next) {
         if (strcmp(x->val.codice, k) == 0) {
             flag = 1;
@@ -307,6 +413,19 @@ void calStat(tabPg_t *tabPg, char k[6+1], tabInv_t *tabInv){
                     x->val.stat.def += x->val.equip.vettEq[i]->stat.def;
                     x->val.stat.mag += x->val.equip.vettEq[i]->stat.mag;
                     x->val.stat.spr += x->val.equip.vettEq[i]->stat.spr;
+                    if (x->val.stat.hp < 0){
+                        x->val.stat.hp = 0;
+                    }if (x->val.stat.mp < 0){
+                        x->val.stat.mp = 0;
+                    }if (x->val.stat.atk < 0){
+                        x->val.stat.atk = 0;
+                    }if (x->val.stat.def < 0){
+                        x->val.stat.def = 0;
+                    }if (x->val.stat.mag < 0){
+                        x->val.stat.mag = 0;
+                    }if (x->val.stat.spr < 0){
+                        x->val.stat.spr = 0;
+                    }
                 }
                 printf("%s %s %s\n"
                        "HP = %d MP = %d ATK = %d  DEF = %d MAG = %d SPR = %d\n", x->val.codice,
@@ -316,5 +435,7 @@ void calStat(tabPg_t *tabPg, char k[6+1], tabInv_t *tabInv){
             }
         }
     }
-
+    if(!flag){
+        printf("Codice non trovato!\n");
+    }
 }
