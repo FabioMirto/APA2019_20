7ea26bfcfb66e66dfec97dfc0c27fbcd0fe07299
FabioMirto <56192516+FabioMirto@users.noreply.github.com>
2019-12-04 19:23:15 +0100

    Create menu.h

---
+++ Lab8/ES3/menu.h
@@ -0,0 +1,8 @@
+#include "inventario.h"
+
+typedef enum {
+    r_newpg, r_deletepg, r_addob, r_deleteob, r_statpg, r_stampainv, r_cercainv, r_stamppg, r_err, r_fine
+} comandi_e;
+
+void selezionaDati(tabPg_t *tabPg, tabInv_t *tabInv, comandi_e codiceComando);
+comandi_e leggiComando(void);
